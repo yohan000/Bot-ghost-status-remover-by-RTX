5ca2f3c1c74bf7c8f13ad04d5ed55d8aa928b239
yohan000 <140971855+yohan000@users.noreply.github.com>
2024-02-08 11:09:37 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Listening to 🎭Anonymous"];
 
 
 let currentIndex = 0;
